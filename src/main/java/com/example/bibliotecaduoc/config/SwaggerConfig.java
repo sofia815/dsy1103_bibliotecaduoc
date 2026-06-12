a1b5e47c14a89784eb7d604aac065250381c5037
sofia815 <sofiaantoniaog@gmail.com>
2026-06-12 11:26:40 -0400

    update06

---
+++ src/main/java/com/example/bibliotecaduoc/config/SwaggerConfig.java
@@ -0,0 +1,10 @@
+package com.example.bibliotecaduoc.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class SwaggerConfig {
+
+}
